196393808ea538267df34975b02d74ad42d7315d
zrummler <zachary.rummler@tufts.edu>
2023-09-01 23:05:34 -0400

    added more styles to footer, and other pages

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("JavaScript is running");
+
+/*
+document.addEventListener("DOMContentLoaded", function () {
+  let prevScrollPos = window.pageYOffset;
+  const header = document.getElementById("header-container");
+
+  window.addEventListener("scroll", () => {
+    let currentScrollPos = window.pageYOffset;
+
+    if (header != null) {
+      if (prevScrollPos > currentScrollPos) {
+        // Scrolling up, show the header
+        console.log("up");
+        header.classList.remove("hidden");
+      } else {
+        // Scrolling down, hide the header
+        console.log("down");
+        header.classList.add("hidden");
+      }
+    } else {
+      console.log("NULL");
+    }
+    prevScrollPos = currentScrollPos;
+  });
+});
+*/
